5ed1a3b63a214fa277f682ca1cf1ae2480bd1e8d
MachineHand <kxmfkdlej@naver.com>
2022-03-22 21:05:29 +0900

    final (summary+errer solution)

--- 2.js
+++ 2.js
@@ -3,17 +3,20 @@ let TodayMoney=0;//현재 넣은돈
 let coffe = document.getElementById('coffe');
 let corn = document.getElementById('corn');
 let water = document.getElementById('water');
-//let product = document.getElementsByClassName('btn');
+//이걸 배열로 만들기->product.product4
 
 let big = document.getElementById('1000')
 let middle = document.getElementById('500');
 let small = document.getElementById('100');
+//이것에 값 집어넣기 ex) big = 1000;
 
-//동일
+
 let product={
+    clientMoney:[1000,500,100],
     price:[700,1000,500],
-    product4:["coffe","corn","water"]
+    product4:["coffe","corn","water"]//문자 빼도 됨.보기 쉽게 하기위해.
 };
+
 product.product4[0]=document.getElementById('coffe');
 product.product4[1]=document.getElementById('corn');
 product.product4[2]=document.getElementById('water');
@@ -23,61 +26,48 @@ product.price[1]=document.getElementById('500');
 product.price[2]=document.getElementById('100');*/
 //동일
 
+// 
 
-//let product = ['coffe','corn','water'];
 
 coffe.onclick=function sumCoffe(){
-    if(700<=TodayMoney){
-    TodayMoney-=700;
-    alert("커피를 샀다");
-    document.getElementById("money").innerHTML="현재금액: "+TodayMoney;
-    }
-    else{
-        alert("돈이 부족하다.");
-    }
+    TodayMoneyCompare(0,"커피");
 }
 corn.onclick=function sumCorn(){
-    if(1000<=TodayMoney){
-    TodayMoney-=1000;
-    alert("옥수수수염차를 샀다.");
-    document.getElementById("money").innerHTML="현재금액: "+TodayMoney;
-    }
-    else{
-        alert("돈이 부족하다.");
-    }
+    TodayMoneyCompare(1,"옥수수수염차");
 }
 water.onclick=function sumWater(){
-    if(500<=TodayMoney){
-        TodayMoney-=500;
-        alert("물을 샀다.");
-        document.getElementById("money").innerHTML="현재금액: "+TodayMoney;
-        }
-        else{
-            alert("돈이 부족하다.");
-        }
-    }
+    TodayMoneyCompare(2,"물");  
+}
+
 
 big.onclick=function sumBig(){
-    TodayMoney+=1000;
-    alert("1000원 넣었다");
-    document.getElementById("money").innerHTML="현재금액: "+TodayMoney;
+    pushMoney(0,"1000")
     changeColor();
 }
 middle.onclick=function sumMiddle(){
-    TodayMoney+=500;
-    alert("500원 넣었다");
-    document.getElementById("money").innerHTML="현재금액: "+TodayMoney;
+    pushMoney(1,"500")
     changeColor();
 }
 small.onclick=function sumSmall(){
-    TodayMoney+=100;
-    alert("100원 넣었다");
-    document.getElementById("money").innerHTML="현재금액: "+TodayMoney;
+    pushMoney(2,"100")
     changeColor();
 }
 
-
 changeColor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function changeColor() {
     for (let i = 0; i < 3; i++) {
@@ -90,3 +80,20 @@ function changeColor() {
     }
 }
 
+function TodayMoneyCompare(vlaue, inform) {
+    if (product.price[vlaue] <= TodayMoney) {
+        TodayMoney -= product.price[vlaue];
+        alert(inform + "을(를) 샀다.");
+        document.getElementById("money").innerHTML = "현재금액: " + TodayMoney;
+        changeColor();
+    }
+    else {
+        alert("돈이 부족하다.");
+    }
+}//현재돈이 가격보다 크면 사고 현재금액 작성 돈 바꾸기
+
+function pushMoney(cM, money) {
+    TodayMoney += product.clientMoney[cM];
+    alert(money + "원 넣었다");
+    document.getElementById("money").innerHTML = "현재금액: " + TodayMoney;
+}//돈 넣고 현재금액 표시 
