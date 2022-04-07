82267847389677a34768ff537fefa5695200b29b
MachineHand <kxmfkdlej@naver.com>
2022-04-07 14:45:47 +0900

    creat TODOLIst

---
+++ ToDoList.js
@@ -0,0 +1,75 @@
+let checkCount=0;
+function ToDoListPlus() {
+    let table = document.getElementById('table');
+    let tr = document.createElement('tr');//tr 만들고
+    for (let i = 0; i < 2; i++) {//두번 반복문 넣어서 td2개 1.체크 2.txt값
+        let td = document.createElement('td');
+        if (i == 0) {//checkbox만들기
+            let checkbox = document.createElement("INPUT");
+            checkbox.setAttribute("type", "checkbox");//id=++num 추가해야함
+            checkbox.setAttribute("class","checkboxes");
+            td.appendChild(checkbox);
+            tr.appendChild(td);
+        }
+        else {//텍스트 값 넣기
+            let txtValue = document.getElementById('txt').value
+            let txt = document.createTextNode(txtValue);//.innerHTML
+            td.appendChild(txt);
+            tr.appendChild(td);
+        }
+    }
+    table.appendChild(tr);
+}
+
+function SD(){
+    let table = document.getElementById('table'); //테이블ID
+    let rowLength = table.rows.length-1;//테이블 tr 수 만큼 들어감.
+    for (let i = rowLength; i >= 1; i--) {//thead에도 tr이 있어서 건너뛰기
+        //let tbody = document.getElementsByTagName('tbody')[i];//tbody[i]가 들어감
+        let check=document.getElementsByClassName('checkboxes')[i-1];//check가들어감
+        let check1=check.checked;
+        if (check1==true){
+            table.deleteRow(i);
+            //tbody삭제
+        }
+    }
+}
+
+
+function LD() {
+    let table = document.getElementById('table'); //테이블ID
+    let rowLength = table.rows.length-1;//테이블 tr 수 만큼 들어감.
+    for (let i = rowLength; i >= 1; i--) {//thead에도 tr이 있어서 건너뛰기
+        table.deleteRow(i);
+        break;
+    }
+}
+
+function AD() {
+    let table = document.getElementById('table'); //테이블ID
+    let rowLength = table.rows.length-1;//테이블 tr 수 만큼 들어감.
+    for (let i = rowLength; i >= 1; i--) {//thead에도 tr이 있어서 건너뛰기
+        table.deleteRow(i);
+    }
+}
+
+
+//let ul = document.getElementById('ul');
+//함수 밖에서는 먹지 않음
+count=0;
+function a() {
+    let ul = document.getElementById('ul');
+    let li = document.createElement('li');
+    let exText = document.createTextNode("추가된 li+" + count);
+    let txtValue = document.getElementById('txt').value
+    let txt = document.createTextNode(txtValue);//.innerHTML
+    let checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");//id=++num 추가해야함
+    //check.setAttribute("id", "num");//id=++num 추가해야함
+    li.appendChild(checkbox);
+    li.appendChild(txt);
+    //li.innerHTML="추가된 li+"+count;
+    li.appendChild(exText);
+    ul.appendChild(li);
+    count++;
+}
--- machine.js
+++ machine.js
@@ -1,81 +1,64 @@
 let TodayMoney=0;//현재 넣은돈
 
-let coffe = document.getElementById('coffe');
-let corn = document.getElementById('corn');
-let water = document.getElementById('water');
-//이걸 배열로 만들기->product.product4
+let product={
+    clientMoney:[1000,500,100],
+    price:[700,1000,500],
+    drink:["","",""]//문자 빼도 됨.보기 쉽게 하기위해.
+};
+
+product.drink[0]=document.getElementById('coffe');
+product.drink[1]=document.getElementById('corn');
+product.drink[2]=document.getElementById('water');
 
 let big = document.getElementById('1000')
 let middle = document.getElementById('500');
 let small = document.getElementById('100');
+
+
 //이것에 값 집어넣기 ex) big = 1000;
 
 
-let product={
-    clientMoney:[1000,500,100],
-    price:[700,1000,500],
-    product4:["coffe","corn","water"]//문자 빼도 됨.보기 쉽게 하기위해.
-};
-
-product.product4[0]=document.getElementById('coffe');
-product.product4[1]=document.getElementById('corn');
-product.product4[2]=document.getElementById('water');
 /*
 product.price[0]=document.getElementById('1000');
 product.price[1]=document.getElementById('500');
 product.price[2]=document.getElementById('100');*/
 //동일
 
-// 
+changeColor();
 
-
-coffe.onclick=function sumCoffe(){
+product.drink[0].onclick=function sumCoffe(){
     TodayMoneyCompare(0,"커피");
 }
-corn.onclick=function sumCorn(){
+product.drink[1].onclick=function sumCorn(){
     TodayMoneyCompare(1,"옥수수수염차");
 }
-water.onclick=function sumWater(){
+product.drink[2].onclick=function sumWater(){
     TodayMoneyCompare(2,"물");  
 }
 
 
 big.onclick=function sumBig(){
-    pushMoney(0,"1000")
+    pushMoney(0,"1000");
     changeColor();
 }
 middle.onclick=function sumMiddle(){
-    pushMoney(1,"500")
+    pushMoney(1,"500");
     changeColor();
 }
 small.onclick=function sumSmall(){
-    pushMoney(2,"100")
+    pushMoney(2,"100");
     changeColor();
 }
-
-changeColor();
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 function changeColor() {
     for (let i = 0; i < 3; i++) {
         if (product.price[i] > TodayMoney) {
-            product.product4[i].style.color = "red"; // [1]=coffee=>red
+            product.drink[i].style.color = "red"; // [1]=coffee=>red
         }
         else if (product.price[i] <= TodayMoney) {
-            product.product4[i].style.color = "blue";
+            product.drink[i].style.color = "blue";
         }
     }
 }
